0e68ed97068af413f6f318b872f87990df52b04c
DaryenFernandez <da.fernandezq@duocuc.cl>
2024-09-01 14:15:32 -0400

    alerta y toast

--- src/app/pages/cambio-p/cambio-p.page.ts
+++ src/app/pages/cambio-p/cambio-p.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-cambio-p',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cambio-p.page.scss'],
 })
 export class CambioPPage implements OnInit {
+  contrasena = '';
+  
+  constructor(
+    public alertController: AlertController,
+    public navCtrl: NavController,
+    public toastController :ToastController,
+  ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  async continuar() {
+    if ( this.contrasena === '') {
+      this.mostrarAlerta('Debe ingresar correo y contraseña');
+    } else {
+      this.presentToast('bottom')
+      this.navCtrl.navigateRoot('/home');
+    }
   }
 
+  async mostrarAlerta(mensaje: string) {
+    const alert = await this.alertController.create({
+      header: 'No se ha podido procesar tu solicitud',
+      message: '¡debe rellenar las casillas! ',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'La Contraseña Se  Actualizo correctamente',
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
 }
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertController, NavController } from '@ionic/angular';
+import { AlertController, NavController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +11,7 @@ export class LoginPage implements OnInit {
   contrasena = '';
 
   constructor(
+    public toastController :ToastController,
     public alertController: AlertController,
     public navCtrl: NavController
   ) {}
@@ -21,7 +22,7 @@ export class LoginPage implements OnInit {
     if (this.correo === '' || this.contrasena === '') {
       this.mostrarAlerta('Debe ingresar correo y contraseña');
     } else {
-      // Navega a la página de inicio
+      this.presentToast('bottom')
       this.navCtrl.navigateRoot('/home');
     }
   }
@@ -34,4 +35,16 @@ export class LoginPage implements OnInit {
     });
     await alert.present();
   }
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'se inicio sesion correctamente',
+      duration: 1500,
+      position: position,
+    });
+
+    await toast.present();
+  }
+
+
+
 }
