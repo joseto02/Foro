266586b673a6b0b7a611c85a3e108686b0d2a68b
joseDeveloper <jom.gonzalez@duocuc.cl>
2024-09-21 19:04:02 -0300

    Se arreglo registro

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -7,49 +7,105 @@ import { AlertController, NavController, ToastController } from '@ionic/angular'
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
-  correo = '';
-  contrasena = '';
-  usuario = '';
-  mail = '';
-  contrasena2 = '';
+
+  // Variables para el formulario
+  usuario: string = '';
+  correo: string = '';
+  correo2: string = '';
+  contrasena: string = '';
+  contrasena2: string = '';
+
+  // Variables para mostrar mensajes de validacion
+  mensajeUsuario: string = '';
+  mensajeCorreo: string = '';
+  msjCorreosIguales: string = '';
+  msjContrasena: string = '';
+  msjContrasenasIguales: string = '';
+
+
   constructor(
     public alertController: AlertController,
     public navCtrl: NavController,
     public toastController :ToastController,
   ) { }
 
-  ngOnInit() {}
-  async continuar() {
-    // if (this.correo === '' || this.contrasena === '' || this.usuario === '' || this.mail === '') {
-    //   this.mostrarAlerta('Debe ingresar correo y contraseña');
-    // } else {
-    //   this.presentToast('bottom')
-    //   this.navCtrl.navigateRoot('/home');
-    // }
+  ngOnInit() { }
+  
+  validarUsuario() {
     if (this.usuario === '') {
-      this.mostrarAlerta('Ingrese un nombre de usuario');
-      return;
+      this.mensajeUsuario = 'El nombre de usuario es obligatorio';
     }
+    else {
+      this.mensajeUsuario = '';
+    }
+  }
+
+  validarCorreo() {
+    const correoValido = /@/;
     if (this.correo === '') {
-      this.mostrarAlerta('Por favor ingrese un correo');
-      return;
+      this.mensajeCorreo = 'El correo es obligatorio';
     }
-    if (!this.correo.includes('@')) {
-      this.mostrarAlerta('Ingrese un correo valido');
-      return;
+    else if (!correoValido.test(this.correo)) {
+      this.mensajeCorreo = 'Ingrese un correo valido';
     }
-    if (this.correo !== this.mail) {
-      this.mostrarAlerta('Los correos no coinciden');
-      return;
+    else {
+      this.mensajeCorreo = '';
     }
+  }
+
+  correosIguales() {
+    if (this.correo !== this.correo2) {
+      this.msjCorreosIguales = 'Los correos no coiciden';
+    }
+    else {
+      this.msjCorreosIguales = '';
+    }
+  }
+
+  validarContrasena() {
+    const contrasenaValida = /[!@#$%^&*(),.?":{}|<>]/;
+    const mayusculaValida = /[A-Z]/;
+
     if (this.contrasena === '') {
-      this.mostrarAlerta('Debe ingresar una contraseña');
-      return;
+      this.msjContrasena = 'Debe ingresar una contraseña';
     }
+    else if (this.contrasena.length < 6) {
+      this.msjContrasena = 'La constraseña debe tener un minimo de 6 caracteres';
+    }
+    else if (!mayusculaValida.test(this.contrasena)) {
+      this.msjContrasena = 'La contraseña debe tener almenos una mayuscula';
+    }
+    else if (!contrasenaValida.test(this.contrasena)) {
+      this.msjContrasena = 'La contraseña debe tener almenos un simbolo';
+    }
+    else {
+      this.msjContrasena = '';
+    }
+  }
+
+  contrasenasIguales() {
     if (this.contrasena !== this.contrasena2) {
-      this.mostrarAlerta('Las contraseñas no coiciden');
+      this.msjContrasenasIguales = 'Las contaseñas no coiciden';
+    }
+    else {
+      this.msjContrasenasIguales = '';
+    }
+  }
+
+  async continuar() {
+
+    // Se ejecutan las validaciones
+
+    this.validarUsuario();
+    this.validarCorreo();
+    this.correosIguales();
+    this.validarContrasena();
+    this.contrasenasIguales();
+
+    if (this.mensajeUsuario !== '' || this.mensajeCorreo !== '' || this.msjCorreosIguales !== '' || this.msjContrasena !== '' || this.msjContrasenasIguales !== '') {
       return;
     }
+
     this.presentToast('bottom');
     this.navCtrl.navigateRoot('/login');
   }
@@ -71,5 +127,6 @@ export class RegistroPage implements OnInit {
 
     await toast.present();
   }
+
 
 }
